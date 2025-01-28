8c953302632fc7b175b65591053cd37b7bfeda9f
pkritt <krittapat37819@gmail.com>
2025-01-28 16:03:04 +0700

    #

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
+void shuffle(int *,int *,int *,int *);
+
+int main(){
+	int a = 50, b = 100, c = 500, d = 1000;
+	
+	srand(time(0));	
+	
+	for(int i = 0;i < 10;i++){
+	    shuffle(&a,&b,&c,&d);
+	    cout << a << " " << b << " " << c << " " << d << endl;
+	}
+	
+	return 0;
+}
+
+//Do not modify source code above this line
+void shuffle(int *a,int *b,int *c,int *d){
+ int arr[] = {*a, *b, *c, *d};
+
+    for (int i = 3; i > 0; --i) {
+        int j =  rand() % (4);
+        swap(arr[i], arr[j]);
+    }
+
+    *a = arr[0];
+    *b = arr[1];
+    *c = arr[2];
+    *d = arr[3];
+}
